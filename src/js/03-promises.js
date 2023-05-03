91dfea09a94213bfae613fc523a3616a54bca7aa
Mykokola <j0kfrbr@gmail.com>
2023-05-02 21:53:23 -0500

    first commit

---
+++ src/js/03-promises.js
@@ -0,0 +1,50 @@
+const delayEl = document.querySelector('[name=delay]'),
+  stepEl = document.querySelector('[name=step]'),
+  amountEl = document.querySelector('[name=amount]'),
+  btnSubmitEl = document.querySelector('[type=submit]');
+
+console.log(delayEl);
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const shouldResolve = Math.random() > 0.3; 
+        if (shouldResolve) { 
+          resolve({position:position,delay:delay})
+        } else { 
+          reject({position:position,delay:delay})
+        } 
+    },delay);
+})
+}
+function numbFix(numb){
+  if(+numb){
+    return +numb
+  }else{
+    return 0
+  }
+}
+
+
+btnSubmitEl.addEventListener('click',(e) => {
+  let delayElValue = delayEl.value,
+      stepElValue = stepEl.value,
+      amountElValue = amountEl.value
+  e.preventDefault()
+  if(numbFix(delayElValue)>1){
+    setTimeout(() => {
+      let startDelay = numbFix(delayElValue),
+          startStep = numbFix(stepElValue)
+      for(let i = 1; i <= numbFix(amountElValue);i++){
+        createPromise(i,startDelay)
+        .then(({position, delay}) => console.log(`Resolved with ${position} and ${delay}`))
+        .catch(({position, delay}) => console.log(`Catched with ${position} and ${delay}`));
+        
+        startDelay += startStep
+      }
+    },numbFix(delayElValue) )
+
+    
+  }
+ 
+
+})
